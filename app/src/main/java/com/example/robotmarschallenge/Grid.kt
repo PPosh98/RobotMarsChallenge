f9b072f648cdfcbfa011275985fc5aaccecded0b
Azim Shah <azim.1998@hotmail.com>
2023-06-19 13:39:05 +0100

    Add scaffolding for classes

---
+++ app/src/main/java/com/example/robotmarschallenge/Grid.kt
@@ -0,0 +1,20 @@
+package com.example.robotmarschallenge
+
+class Grid(
+    val width: Int,
+    val height: Int,
+    val scents: MutableMap<Pair<Int, Int>, Boolean> = mutableMapOf()
+) {
+
+    fun checkBounds(x: Int, y: Int): Boolean {
+
+    }
+
+    fun checkScent(x: Int, y: Int): Boolean {
+
+    }
+
+    fun markScent(x: Int, y: Int) {
+
+    }
+}
---
+++ app/src/main/java/com/example/robotmarschallenge/Robot.kt
@@ -0,0 +1,26 @@
+package com.example.robotmarschallenge
+
+class Robot(
+    var x: Int,
+    var y: Int,
+    var orientation: Char,
+    var instructions: String,
+    var status: String = ""
+) {
+
+    fun executeInstruction(instruction: Char, grid: Grid) {
+
+    }
+
+    fun turnLeft() {
+
+    }
+
+    fun turnRight() {
+
+    }
+
+    fun moveForward(grid: Grid) {
+
+    }
+}
